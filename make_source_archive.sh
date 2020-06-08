78940702b00a605b328e702a1734d29aaff2c13a
Maximilian Gaukler <max.gaukler@fau.de>
2020-06-08 10:06:33 +0200

    Switch source archive format to tar.gz
    Not all ZIP unpackers correctly support the x-bit, but it is important for the shell scripts to work.

--- make_source_archive.sh
+++ make_source_archive.sh
@@ -4,5 +4,5 @@
 # Requirement: All submodules properly initialized (git submodule update --init --recursive)
 set -e
 cd "$(dirname "$0")"
-./git-archive-all/git-archive-all.sh --format zip -- source-complete.zip
-ls -l --si source-complete.zip
+./git-archive-all/git-archive-all.sh --format tar.gz -- source-complete.tar.gz
+ls -l --si source-complete.tar.gz
