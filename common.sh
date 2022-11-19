61690883978e96d712a00cfcfc0fe1d7a501289e
Maximilian Gaukler <max.gaukler@fau.de>
2022-11-19 11:52:49 +0100

    show git rev in logfile

--- common.sh
+++ common.sh
@@ -4,4 +4,5 @@ set -e
 cd "$(dirname "$0")"
 test -f hyst/README.md || { echo "Cannot find the hyst submodule, try 'git submodule update --init --recursive'"; exit 1; }
 test -f mpmath/README.rst || { echo "Cannot find the mpmath submodule, try 'git submodule update --init --recursive'"; exit 1; }
-
+echo "GIT Revision: "
+git describe --always --dirty
