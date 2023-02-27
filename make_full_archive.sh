a13239392cd847c67d32fdcee2a89b103570b5e7
Maximilian Gaukler <max.gaukler@fau.de>
2023-02-27 09:14:44 +0100

    Fix cleanup when generating output archive

--- make_full_archive.sh
+++ make_full_archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-git clean -xi || echo "Warning: git clean failed, possibly because you are running from the output archive and not from a GIT repository. Please cleanup the previous output files yourself."
+git clean -xdi || echo "Warning: git clean failed, possibly because you are running from the output archive and not from a GIT repository. Please cleanup the previous output files yourself."
 ./run_experiments_docker.sh "$@"
 rm output.tar.gz || true
 tar --exclude=systems.pickle --exclude='*.pyc' --exclude=output.tar.gz --exclude-vcs -czvf output.tar.gz .
